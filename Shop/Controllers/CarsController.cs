2747751d2237267889ad2daf9e1c5a2c7d84dd6a
Hedgehog229 <ao4ever@mail.ru>
2021-08-08 17:45:28 +0300

    End Age9 realisation

--- Shop/Controllers/CarsController.cs
+++ Shop/Controllers/CarsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shop.Data.Interfaces;
+using Shop.Data.Models;
 using Shop.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -19,13 +20,49 @@ namespace Shop.Controllers
             _allCategory = iCateg;
         }
 
-        public ViewResult List() 
+        [Route("Cars/List")]
+        [Route("Cars/List/{category}")] //можно добавлять больше параметров, например:  [Route("Cars/List/{category}/{id}")], но в функции должен быть этот параметр public ViewResult List(string category, int id) 
+        public ViewResult List(string category) 
         {
+            string _category = category;
+            IEnumerable<Car> cars = null;
+            string currCategory ="";
+
+            if (string.IsNullOrEmpty(category))
+            {
+                cars = _allCars.Cars.OrderBy(i => i.Id);
+            }
+            else
+            {                                        //StringComparison.OrdinalIgnoreCase - параметр для игнорирования регистра в передаваемой строке
+                if (string.Equals("electro", category, StringComparison.OrdinalIgnoreCase))
+                {
+                    cars = _allCars.Cars.Where(i => i.Category.CategoryName.Equals("Электромобили")).OrderBy(i => i.Id);
+                    currCategory = "Электромобили";
+                }
+                else 
+                {
+                    if (string.Equals("fuel", category, StringComparison.OrdinalIgnoreCase))
+                    {
+                        cars = _allCars.Cars.Where(i => i.Category.CategoryName.Equals("Классический автомобиль")).OrderBy(i => i.Id);
+                        currCategory = "Классический автомобиль";
+                    }
+                }
+
+                //currCategory = _category;               
+            }
+
+            var carObj = new CarListViewModel()
+            {
+                AllCars = cars,
+                CurrCategory = currCategory
+            };
+
             ViewBag.Title = "Страница с автомобилями";
-            CarListViewModel obj = new CarListViewModel();
-            obj.AllCars = _allCars.Cars;
-            obj.CurrCategory = "Автомобили";
-            return View(obj);
+            //CarListViewModel obj = new CarListViewModel();
+            //obj.AllCars = _allCars.Cars;
+            //obj.CurrCategory = "Автомобили";
+            //return View(obj);
+            return View(carObj);
         }
     }
 }
