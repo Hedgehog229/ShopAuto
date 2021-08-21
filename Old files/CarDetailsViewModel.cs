8964d64e0ebc5de54319d22ea8a8a2737e9ed06e
Hedgehog229 <ao4ever@mail.ru>
2021-08-21 15:39:20 +0300

    Summary

---
+++ Old files/CarDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.ViewModels
+{
+    public class CarDetailsViewModel
+    {
+        public CarDetails _carDetails { get; set; }
+        public int IdCar { get; set; }
+    }
+}
